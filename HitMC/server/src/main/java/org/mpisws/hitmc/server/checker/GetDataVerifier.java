118a903373669f5f90bd49cdf45d654d49c8935e
Lexus <lingzhi.ouyang@outlook.com>
2022-05-17 15:58:57 +0800

    add checker GetDataVerifier

---
+++ HitMC/server/src/main/java/org/mpisws/hitmc/server/checker/GetDataVerifier.java
@@ -0,0 +1,43 @@
+package org.mpisws.hitmc.server.checker;
+
+import org.mpisws.hitmc.api.NodeState;
+import org.mpisws.hitmc.api.state.Vote;
+import org.mpisws.hitmc.server.TestingService;
+import org.mpisws.hitmc.server.statistics.Statistics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class GetDataVerifier implements Verifier{
+
+    private static final Logger LOG = LoggerFactory.getLogger(GetDataVerifier.class);
+
+    private final TestingService testingService;
+    private final Statistics statistics;
+
+    public GetDataVerifier(final TestingService testingService, Statistics statistics) {
+        this.testingService = testingService;
+        this.statistics = statistics;
+    }
+
+    @Override
+    public boolean verify() {
+        List<Integer> returnedZxidList = testingService.getReturnedZxid();
+        final int len = returnedZxidList.size();
+        assert len >= 2;
+        final int latestOne = returnedZxidList.get(len - 1);
+        final int latestSecond = returnedZxidList.get(len - 2);
+        boolean result = latestOne >= latestSecond;
+        if (result) {
+            LOG.debug("SUC");
+            statistics.reportResult("MONOTONIC_READ:SUCCESS");
+            return true;
+        }
+        else {
+            LOG.debug("FAIL");
+            statistics.reportResult("MONOTONIC_READ:FAILURE");
+            return false;
+        }
+    }
+}
