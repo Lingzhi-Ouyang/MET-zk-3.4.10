418d8249e0127c57e6405cbc1d101ddf6ed01f15
Lexus <lingzhi.ouyang@outlook.com>
2022-05-17 10:31:28 +0800

    update ClientProxy

--- HitMC/server/src/main/java/org/mpisws/hitmc/server/executor/ClientProxy.java
+++ HitMC/server/src/main/java/org/mpisws/hitmc/server/executor/ClientProxy.java
@@ -90,7 +90,7 @@ public class ClientProxy extends Thread{
     }
 
     public void shutdown(){
-        LOG.debug("---shutting down zookeeper client proxy thread {}", currentThread().getName());
+        LOG.debug("---shutting down zookeeper client proxy thread {}", this.getName());
         this.ready = false;
         this.stop = true;
     }
