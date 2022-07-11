0ff730210f4e9851c9f1b1fd46d88566c5d8715d
Lexus <lingzhi.ouyang@outlook.com>
2022-07-11 15:58:40 +0800

    add Trace.java

--- HitMC/server/src/main/java/org/mpisws/hitmc/server/scheduler/Trace.java
+++ HitMC/server/src/main/java/org/mpisws/hitmc/server/scheduler/Trace.java
@@ -1,27 +1,58 @@
 package org.mpisws.hitmc.server.scheduler;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.zookeeper.server.quorum.LearnerHandlerAspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Trace {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Trace.class);
+
     private final String traceName;
-    private final List<String> executionSteps = new ArrayList<>();
+    private final JSONArray executionSteps = new JSONArray();
     private int currentIdx;
     private int stepCount;
+    private int serverNum;
+    private List<String> serverIds;
 
-    public Trace(String traceName) {
+    public Trace(String traceName, int serverNum, List<String> serverIds, JSONArray jsonArray) {
         this.traceName = traceName;
+        this.serverNum = serverNum;
+        this.serverIds = serverIds;
         this.currentIdx = -1;
-        this.stepCount = 0;
+        this.stepCount = jsonArray.size();
+        LOG.debug("stepCount: " + stepCount);
+        this.executionSteps.addAll(jsonArray);
     }
 
     public String getTraceName() {
         return traceName;
     }
 
-    public int getStepNum() {
+    public int getServerNum() {
+        return serverNum;
+    }
+
+    public List<String> getServerIds() {
+        return serverIds;
+    }
+
+    public int getStepCount() {
         return stepCount;
+    }
+
+    public void setStepCount(int stepCount) {
+        this.stepCount = stepCount;
+    }
+
+    public void addSteps(JSONArray eventList) {
+        executionSteps.addAll(eventList);
     }
 
     public void addStep(String event) {
@@ -29,11 +60,21 @@ public class Trace {
         stepCount++;
     }
 
-    public String nextStep() {
+    public JSONObject nextStep() {
         this.currentIdx++;
+        LOG.debug("currentIdx: " + currentIdx + ", stepCount: " + stepCount);
         assert currentIdx < stepCount;
-        return executionSteps.get(currentIdx);
+        return (JSONObject) executionSteps.get(currentIdx);
     }
+
+    public JSONObject getStep(int idx) {
+        this.currentIdx = idx;
+        LOG.debug("step: " + (currentIdx+1) + ", currentIdx: " + currentIdx + ", stepCount: " + stepCount);
+        assert currentIdx < stepCount;
+        return (JSONObject) executionSteps.get(currentIdx);
+    }
+
+
 
     @Override
     public String toString() {
