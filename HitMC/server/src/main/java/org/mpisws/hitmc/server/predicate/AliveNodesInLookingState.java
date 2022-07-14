9e5ff322592125105030232c20719d224b0ef05c
Lexus <lingzhi.ouyang@outlook.com>
2022-07-14 11:59:41 +0800

    update AliveNodesInLookingState

--- HitMC/server/src/main/java/org/mpisws/hitmc/server/predicate/AliveNodesInLookingState.java
+++ HitMC/server/src/main/java/org/mpisws/hitmc/server/predicate/AliveNodesInLookingState.java
@@ -8,54 +8,78 @@ import org.mpisws.hitmc.server.state.Subnode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class AliveNodesInLookingState implements WaitPredicate{
 
     private static final Logger LOG = LoggerFactory.getLogger(AliveNodesInLookingState.class);
 
     private final TestingService testingService;
 
+    private final List<Integer> peers;
+
     public AliveNodesInLookingState(final TestingService testingService) {
         this.testingService = testingService;
+        peers = null;
     }
+
+    public AliveNodesInLookingState(final TestingService testingService, final List<Integer> peers) {
+        this.testingService = testingService;
+        this.peers = peers;
+    }
+
+
 
     @Override
     public boolean isTrue() {
-        for (int nodeId = 0; nodeId < testingService.getSchedulerConfiguration().getNumNodes(); ++nodeId) {
-            final NodeState nodeState = testingService.getNodeStates().get(nodeId);
-            LeaderElectionState leaderElectionState = testingService.getLeaderElectionStates().get(nodeId);
-            switch (nodeState) {
-                case STARTING:
-                case STOPPING:
-                    LOG.debug("------Not steady-----Node {} status: {}\n", nodeId, nodeState);
-                    return false;
-                case ONLINE:
-                    if (!LeaderElectionState.LOOKING.equals(leaderElectionState)) {
-                        LOG.debug("------Not steady-----Node {} leaderElectionState: {}\n",
-                                nodeId, leaderElectionState);
-                        return false;
-                    }
-                    LOG.debug("-----------Node {} status: {}", nodeId, nodeState);
-                    break;
-                case OFFLINE:
-                    LOG.debug("-----------Node {} status: {}", nodeId, nodeState);
+        if (peers != null) {
+            for (Integer nodeId : peers) {
+                if (checkNodeNotLooking(nodeId)) return false;
             }
-            for (final Subnode subnode: testingService.getSubnodeSets().get(nodeId)) {
-                if (SubnodeState.PROCESSING.equals(subnode.getState())) {
-                    LOG.debug("------Not steady-----Node {} subnode {} status: {}, subnode type: {}\n",
-                            nodeId, subnode.getId(), subnode.getState(), subnode.getSubnodeType());
-                    return false;
-                }
-                else {
-                    LOG.debug("-----------Node {} subnode {} status: {}, subnode type: {}",
-                            nodeId, subnode.getId(), subnode.getState(), subnode.getSubnodeType());
-                }
+        } else {
+            for (int nodeId = 0; nodeId < testingService.getSchedulerConfiguration().getNumNodes(); ++nodeId) {
+                if (checkNodeNotLooking(nodeId)) return false;
             }
         }
         return true;
     }
 
+    private boolean checkNodeNotLooking(Integer nodeId) {
+        final NodeState nodeState = testingService.getNodeStates().get(nodeId);
+        LeaderElectionState leaderElectionState = testingService.getLeaderElectionStates().get(nodeId);
+        switch (nodeState) {
+            case STARTING:
+            case STOPPING:
+                LOG.debug("------Not steady-----Node {} status: {}\n", nodeId, nodeState);
+                return true;
+            case ONLINE:
+                if (!LeaderElectionState.LOOKING.equals(leaderElectionState)) {
+                    LOG.debug("------Not steady-----Node {} leaderElectionState: {}\n",
+                            nodeId, leaderElectionState);
+                    return true;
+                }
+                LOG.debug("-----------Node {} status: {}", nodeId, nodeState);
+                break;
+            case OFFLINE:
+                LOG.debug("-----------Node {} status: {}", nodeId, nodeState);
+        }
+        for (final Subnode subnode: testingService.getSubnodeSets().get(nodeId)) {
+            if (SubnodeState.PROCESSING.equals(subnode.getState())) {
+                LOG.debug("------Not steady-----Node {} subnode {} status: {}, subnode type: {}\n",
+                        nodeId, subnode.getId(), subnode.getState(), subnode.getSubnodeType());
+                return true;
+            }
+            else {
+                LOG.debug("-----------Node {} subnode {} status: {}, subnode type: {}",
+                        nodeId, subnode.getId(), subnode.getState(), subnode.getSubnodeType());
+            }
+        }
+        return false;
+    }
+
     @Override
     public String describe() {
-        return "all nodes in LOOKING state";
+        if (peers == null) return "all nodes in LOOKING state";
+        else return peers + " in LOOKING state";
     }
 }
