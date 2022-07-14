9c8e831dce9352dc4214f6132637e1eb3b84ae03
Lexus <lingzhi.ouyang@outlook.com>
2022-07-14 11:59:08 +0800

    update AllNodesVoted

--- HitMC/server/src/main/java/org/mpisws/hitmc/server/predicate/AllNodesVoted.java
+++ HitMC/server/src/main/java/org/mpisws/hitmc/server/predicate/AllNodesVoted.java
@@ -5,6 +5,8 @@ import org.mpisws.hitmc.server.TestingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 /***
  * Wait Predicate for the end of an execution.
  */
@@ -14,17 +16,35 @@ public class AllNodesVoted implements WaitPredicate {
 
     private final TestingService testingService;
 
+    private final List<Integer> participants;
+
     public AllNodesVoted(final TestingService testingService) {
         this.testingService = testingService;
+        this.participants = null;
+    }
+
+    public AllNodesVoted(final TestingService testingService, final List<Integer> participants) {
+        this.testingService = testingService;
+        this.participants = participants;
     }
 
     @Override
     public boolean isTrue() {
-        for (int nodeId = 0; nodeId < testingService.getSchedulerConfiguration().getNumNodes(); ++nodeId) {
-            LOG.debug("nodeId: {}, state: {}, votes: {}", nodeId, testingService.getNodeStates().get(nodeId), testingService.getVotes().get(nodeId));
-            if (!NodeState.OFFLINE.equals(testingService.getNodeStates().get(nodeId))
-                    && (!NodeState.ONLINE.equals(testingService.getNodeStates().get(nodeId)) || testingService.getVotes().get(nodeId) == null)) {
-                return false;
+        if ( participants != null) {
+            for (Integer nodeId: participants) {
+                LOG.debug("nodeId: {}, state: {}, votes: {}", nodeId, testingService.getNodeStates().get(nodeId), testingService.getVotes().get(nodeId));
+                if (!NodeState.OFFLINE.equals(testingService.getNodeStates().get(nodeId))
+                        && (!NodeState.ONLINE.equals(testingService.getNodeStates().get(nodeId)) || testingService.getVotes().get(nodeId) == null)) {
+                    return false;
+                }
+            }
+        } else {
+            for (int nodeId = 0; nodeId < testingService.getSchedulerConfiguration().getNumNodes(); ++nodeId) {
+                LOG.debug("nodeId: {}, state: {}, votes: {}", nodeId, testingService.getNodeStates().get(nodeId), testingService.getVotes().get(nodeId));
+                if (!NodeState.OFFLINE.equals(testingService.getNodeStates().get(nodeId))
+                        && (!NodeState.ONLINE.equals(testingService.getNodeStates().get(nodeId)) || testingService.getVotes().get(nodeId) == null)) {
+                    return false;
+                }
             }
         }
         return true;
@@ -32,6 +52,7 @@ public class AllNodesVoted implements WaitPredicate {
 
     @Override
     public String describe() {
-        return "allNodesVoted";
+        if (participants != null) return participants + " voted";
+        else return "allNodesVoted";
     }
 }
