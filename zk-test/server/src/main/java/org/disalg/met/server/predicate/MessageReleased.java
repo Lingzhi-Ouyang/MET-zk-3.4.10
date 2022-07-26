8ccddb76b98e17e83bd8dda8564b281d7ba01392
Lexus <lingzhi.ouyang@outlook.com>
2022-07-27 00:53:32 +0800

    debug predicate MessageReleased

--- zk-test/server/src/main/java/org/disalg/met/server/predicate/MessageReleased.java
+++ zk-test/server/src/main/java/org/disalg/met/server/predicate/MessageReleased.java
@@ -1,6 +1,7 @@
 package org.disalg.met.server.predicate;
 
 import org.disalg.met.api.NodeState;
+import org.disalg.met.api.SubnodeState;
 import org.disalg.met.server.TestingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,33 +18,33 @@ public class MessageReleased implements WaitPredicate {
 
     private final int msgId;
     private final int sendingNodeId;
-    private final Integer receivingNodeId;
+    private final Integer sendingSubnodeId;
 
     public MessageReleased(final TestingService testingService, int msgId, int sendingNodeId) {
         this.testingService = testingService;
         this.msgId = msgId;
         this.sendingNodeId = sendingNodeId;
-        this.receivingNodeId = null;
+        this.sendingSubnodeId = null;
     }
 
-    public MessageReleased(final TestingService testingService, int msgId, int sendingNodeId, int receivingNodeId) {
+    public MessageReleased(final TestingService testingService, int msgId, int sendingNodeId, int sendingSubnodeId) {
         this.testingService = testingService;
         this.msgId = msgId;
         this.sendingNodeId = sendingNodeId;
-        this.receivingNodeId = receivingNodeId;
+        this.sendingSubnodeId = sendingSubnodeId;
     }
 
 
 
     @Override
     public boolean isTrue() {
-        if (receivingNodeId == null) {
+        if (sendingSubnodeId == null) {
             return testingService.getMessageInFlight() == msgId ||
                     NodeState.STOPPING.equals(testingService.getNodeStates().get(sendingNodeId));
         } else {
             return testingService.getMessageInFlight() == msgId ||
                     NodeState.STOPPING.equals(testingService.getNodeStates().get(sendingNodeId)) ||
-                    NodeState.STOPPING.equals(testingService.getNodeStates().get(receivingNodeId));
+                    SubnodeState.UNREGISTERED.equals(testingService.getSubnodes().get(sendingSubnodeId).getState());
         }
     }
 
