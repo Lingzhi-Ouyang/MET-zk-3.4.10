886e548b9793d14db642be1af1d70d2def1f8e8a
Lexus <lingzhi.ouyang@outlook.com>
2022-08-20 19:59:42 +0800

    update predicate MessageReleased

--- zk-test/server/src/main/java/org/disalg/met/server/predicate/MessageReleased.java
+++ zk-test/server/src/main/java/org/disalg/met/server/predicate/MessageReleased.java
@@ -2,7 +2,9 @@ package org.disalg.met.server.predicate;
 
 import org.disalg.met.api.NodeState;
 import org.disalg.met.api.SubnodeState;
+import org.disalg.met.api.TestingDef;
 import org.disalg.met.server.TestingService;
+import org.disalg.met.server.event.Event;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,32 +21,55 @@ public class MessageReleased implements WaitPredicate {
     private final int msgId;
     private final int sendingNodeId;
     private final Integer sendingSubnodeId;
+    private final Event event;
 
-    public MessageReleased(final TestingService testingService, int msgId, int sendingNodeId) {
+    public MessageReleased(final TestingService testingService, final int msgId, final int sendingNodeId) {
         this.testingService = testingService;
         this.msgId = msgId;
         this.sendingNodeId = sendingNodeId;
         this.sendingSubnodeId = null;
+        this.event = null;
     }
 
-    public MessageReleased(final TestingService testingService, int msgId, int sendingNodeId, int sendingSubnodeId) {
+    public MessageReleased(final TestingService testingService,
+                           final int msgId,
+                           final int sendingNodeId,
+                           final int sendingSubnodeId) {
         this.testingService = testingService;
         this.msgId = msgId;
         this.sendingNodeId = sendingNodeId;
         this.sendingSubnodeId = sendingSubnodeId;
+        this.event = null;
+    }
+
+    public MessageReleased(final TestingService testingService,
+                           final int msgId,
+                           final int sendingNodeId,
+                           final int sendingSubnodeId,
+                           final Event event) {
+        this.testingService = testingService;
+        this.msgId = msgId;
+        this.sendingNodeId = sendingNodeId;
+        this.sendingSubnodeId = sendingSubnodeId;
+        this.event = event;
     }
 
 
 
     @Override
     public boolean isTrue() {
-        if (sendingSubnodeId == null) {
+        if (event != null) {
             return testingService.getMessageInFlight() == msgId ||
-                    NodeState.STOPPING.equals(testingService.getNodeStates().get(sendingNodeId));
-        } else {
+                    NodeState.STOPPING.equals(testingService.getNodeStates().get(sendingNodeId)) ||
+                    event.getFlag() == TestingDef.RetCode.NODE_PAIR_IN_PARTITION;
+        }
+        if (sendingSubnodeId != null) {
             return testingService.getMessageInFlight() == msgId ||
                     NodeState.STOPPING.equals(testingService.getNodeStates().get(sendingNodeId)) ||
                     SubnodeState.UNREGISTERED.equals(testingService.getSubnodes().get(sendingSubnodeId).getState());
+        } else {
+            return testingService.getMessageInFlight() == msgId ||
+                    NodeState.STOPPING.equals(testingService.getNodeStates().get(sendingNodeId));
         }
     }
 
